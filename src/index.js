995c7e891f58af8a619ca89716a6f5f98523379f
Kwangyoung <kwangyoung.kim@gmail.com>
2020-07-14 13:37:58 +0900

    Completed module8

--- src/index.js
+++ src/index.js
@@ -32,16 +32,13 @@ async function getBooksAndMoviesAsync() {
     return { books, movies };
   } catch (error) {
     console.log("Error fetching books and movies", error);
+    return error;
   }
 }
 
 async function getBooksOrMoviesAsync() {
-  try {
-    const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
-    return values;
-  } catch (error) {
-    console.error("Error waiting for the promise race", error);
-  }
+  const values = await Promise.race([asyncFetchBookss(), asyncFetchMovies()]);
+  return values;
 }
 
 getBooksAndMoviesAsync()
@@ -50,6 +47,9 @@ getBooksAndMoviesAsync()
       movies: results.movies,
       books: results.books
     });
+  })
+  .catch(error => {
+    console.error("Error in getBooksAndMoviesAsync execution", error);
   });
 
 getBooksOrMoviesAsync()
